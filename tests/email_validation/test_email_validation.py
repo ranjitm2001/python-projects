f80d2c66b2e3db41d641e3e8be83d599e28bcbf6
ranjith5581 <ranjith@kognitos.com>
2023-07-16 03:26:30 +0530

    test-cases for validations - regex function

--- tests/email_validation/test_email_validation.py
+++ tests/email_validation/test_email_validation.py
@@ -1,5 +1,5 @@
 import unittest
-from src.email_validation.email_validation import validator
+from src.email_validation.email_validation import validator, validator_regex
 
 
 class TestEmailValidation(unittest.TestCase):
@@ -13,3 +13,14 @@ class TestEmailValidation(unittest.TestCase):
         assert validator("ran_jith_123@gmail.in") is True
         assert validator("ranjith 123@gmail.in") is False
         assert validator("ranJith@gmail.com") is False
+
+    def test_email_validations_regex(self):
+        assert validator_regex("r@.in") is False
+        assert validator_regex("1anjith@gmail.com") is False
+        assert validator_regex("ran@jith@gmail.com") is False
+        assert validator_regex("ranjith@gmai.lcom") is False
+        assert validator_regex("ran_jith_@gmail.in") is True
+        assert validator_regex("ran.jith@gmail.in") is True
+        assert validator_regex("ran_jith_123@gmail.in") is True
+        assert validator_regex("ranjith 123@gmail.in") is False
+        assert validator_regex("ranJith@gmail.com") is False
